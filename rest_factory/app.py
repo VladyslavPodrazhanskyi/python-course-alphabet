1fcc72738f65d793f4d40174a7a039fd948b1cb5
PVV <vladislav304304@gmail.com>
2019-06-09 22:08:43 +0300

    study flask rest api

--- rest_factory/app.py
+++ rest_factory/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 from config import run_config
 
 app = Flask(__name__)
@@ -55,10 +55,86 @@ class Companies(Resource):
         a.remove(value)
         return "Element deleted successfully"
 
+# Data formatting
+
+a = ["carp", "Dorado", "Steelhead"]
+
+class Fish:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self._super_private = "eatable"
+
+
+
+class Upper(fields.Raw):
+    def format(self, value):
+        return value.upper()
+
+
+
+
+
+
+structure_fish = {
+    "name": Upper,
+    "size": fields.String,
+    "public": fields.String(attribute="_super_private"),
+    "default": fields.String(default="river fish")
+}
+# чтобы скрыть название закрытого метода ( в структуре указывается его имя для публики, а в атрибуте настоящее имя поля)
+# также можно задавать дефолтное значения, если токого поля в экземпляре класса не будет.
+
+class GetFish(Resource):
+    @marshal_with(structure_fish)
+    def get(self):
+        carp = Fish("carp", "200gr")
+        return carp
+
+
+class House:
+    def __init__(self, owner, country, city, street, number):
+        self.owner = owner
+        self.country = country
+        self.city = city
+        self.street = street
+        self.number = number
+        self.residents = []
+        self.family = {"mother": "Nataly", "father": "Vladimir"}
+
+
+structure_family = {
+    "mother": fields.String,
+    "father": fields.String
+}
+
+structure_house = {
+    "owner": fields.String,
+    "address": {
+        "country": fields.String,
+        "city": fields.String,
+        "street": fields.String,
+        "number": fields.Integer},
+    "residents": fields.List(fields.String),
+    "family": fields.Nested(structure_family)
+}
+
+class GetHouse(Resource):
+    @marshal_with(structure_house)
+    def get(self):
+        my_house = House("PVV", "Ukraine", "Kharkiv", "Grushevskogo", 5)
+        my_house.residents = ["me", "my cat"]
+        return my_house
 
 
 api.add_resource(HelloRest, "/")
 api.add_resource(Companies, "/companies", "/companies/<value>")
+api.add_resource(GetFish, "/fishes")
+api.add_resource(GetHouse, "/house")
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
