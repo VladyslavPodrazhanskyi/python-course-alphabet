be16bd7b87c21d9c09d64528d97ed8a2f329e83e
PVV <vladislav304304@gmail.com>
2019-07-14 10:38:49 +0300

    saved changes 14.07

---
+++ tests/test_hw/test_hw.py
@@ -0,0 +1,10 @@
+import unittest
+from objects_and_classes.homework.homework import Car, Garage, Cesar
+
+class CarTest(unittest.TestCase):
+
+
+
+if __name__ == "__main__":
+    if __name__ == '__main__':
+        unittest.main()
